4650ebcec6a9147dcc8dcda37b119e5edd45aa2d
Felix <maier.felix96@gmail.com>
2016-07-04 16:35:51 +0200

    Update playground

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
 var src = 
-`var a = 10;`;
+`// **
+infix operator ** {
+  associativity left
+  precedence 160
+}
+func ** (left: Double, right: Double) -> Double {
+  return pow(left, right);
+}
+
+// %%
+infix operator %% {
+  associativity left
+  precedence 2
+}
+func %%(a: Int, b: Int) -> Int {
+  return (a * b);
+}
+
+var res1 = 2 ** 8;
+expect(res1 == 256.0);
+
+var res2 = 4 % 2 + 27 %% 123;
+expect(res2 == 3321);
+print(SomeClass.workaroundClassVariable);
+`;
 
 swift.innerHTML = src;
 
